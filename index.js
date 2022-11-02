5d30441a67fdf31d28f23d60c76ea252aa500be1
Rachel <racheljhumes@gmail.com>
2022-11-02 11:13:17 -0600

    commit tree 56663cd35a74d4c4af4b8d7a1b38772142f1d7fc

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+
+document.addEventListener('keydown', (e) => {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+})
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`
+  }
+}
+
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left < 360) {
+    dodger.style.left = `${left + 1}px`
+  }
+}
+
+document.addEventListener('keydown', (e) => {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+})
